b850963ba2d15a19bd108e91f8f664848a221f81
Takagi Tasuku <tasuren@outlook.jp>
2025-11-11 22:32:00 +0900

    feat: add env_logger crate to print logs

--- crates/memex-app/src/system.rs
+++ crates/memex-app/src/system.rs
@@ -4,6 +4,8 @@ use memex_backend::{SystemContext, data::AppPath};
 use crate::setup_window;
 
 pub fn boot() {
+    init_log();
+
     let app = Application::new();
 
     let path = futures_lite::future::block_on(AppPath::new(crate::APP_IDENTIFIER.to_owned()))
@@ -12,27 +14,38 @@ pub fn boot() {
 
     app.with_assets(crate::foundation::Assets)
         .run(move |cx: &mut App| {
-            let mut event_loop = match memex_cef::boot().expect("CEFの起動処理に失敗しました。")
-            {
-                Some(event_loop) => event_loop,
-                None => std::process::exit(0),
-            };
-
-            cx.on_app_quit(|_cx| async { on_app_quit() }).detach();
-
-            cx.set_global(system);
+            init_cef(cx, system);
             init_ui(cx);
 
             setup_window(cx).expect("ウィンドウの作成に失敗しました。");
-            cx.spawn(async move |_| {
-                event_loop.start(|task| task()).await;
-            })
-            .detach();
-
             cx.activate(true);
         });
 }
 
+#[inline]
+pub fn init_log() {
+    env_logger::init();
+}
+
+#[inline]
+pub fn init_cef(cx: &mut App, system: SystemContext) {
+    let mut event_loop = match memex_cef::boot().expect("CEFの起動処理に失敗しました。")
+    {
+        Some(event_loop) => event_loop,
+        None => std::process::exit(0),
+    };
+    cx.set_global(system);
+
+    cx.spawn(async move |_| {
+        event_loop.start(|task| task()).await;
+    })
+    .detach();
+
+    cx.on_app_quit(|_cx| async { memex_cef::teardown() })
+        .detach();
+}
+
+#[inline]
 pub fn init_ui(cx: &mut App) {
     #[cfg(target_os = "macos")]
     crate::platform_impl::mac::application::impl_cef_protocol_for_gpui_app();
@@ -43,8 +56,4 @@ pub fn init_ui(cx: &mut App) {
 
     // アプリが直接使うUIの初期化。
     crate::ui::init_workspace_list(cx);
-}
-
-pub fn on_app_quit() {
-    memex_cef::teardown();
 }
