c5ee4158dc9ce55469c9b2f46ae50e12760e33f4
Takagi Tasuku <tasuren@outlook.jp>
2025-12-08 13:43:58 +0900

    feat: add the session id to `BrowserSession`

--- crates/memex-cef/src/session.rs
+++ crates/memex-cef/src/session.rs
@@ -1,15 +1,26 @@
 use cef::Client;
+use uuid::Uuid;
 
 use crate::cef_impl::ClientService;
 
+pub type SessionId = Uuid;
+
 pub struct BrowserSession {
+    id: SessionId,
     pub(crate) client: Client,
 }
 
 impl Default for BrowserSession {
     fn default() -> Self {
         Self {
+            id: Uuid::new_v4(),
             client: ClientService::create(),
         }
+    }
+}
+
+impl BrowserSession {
+    pub fn id(&self) -> SessionId {
+        self.id
     }
 }
